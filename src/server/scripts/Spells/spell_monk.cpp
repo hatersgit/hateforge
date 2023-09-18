961ba6d9cd2327631e468ac49fc1a091a004d8c7
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2023-09-18 18:43:26 +0200

    Add files via upload

---
+++ src/server/scripts/Spells/spell_monk.cpp
@@ -0,0 +1,14 @@
+#include "Player.h"
+#include "ScriptMgr.h"
+#include "SpellAuraEffects.h"
+#include "SpellInfo.h"
+#include "SpellMgr.h"
+#include "SpellScript.h"
+
+enum MonkSpells
+{
+};
+
+void AddSC_monk_spell_scripts()
+{
+}

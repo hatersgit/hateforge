f8a69dbdb7bdc207b78c95ce47c267a726c830a5
hater <hater.rip@proton.me>
2023-11-20 10:26:51 -0500

    update sql

--- modules/mod-Forge/sql/world/updates/20231115 update tabs to add description.sql
+++ modules/mod-Forge/sql/world/updates/20231115 update tabs to add description.sql
@@ -1,2 +1,3 @@
-Alter table `acore_world`.`forge_talent_tabs` add `description` VARCHAR(255) after `background`;
+Alter table `acore_world`.`forge_talent_tabs` add `description` VARCHAR(1000) after `background`;
 Alter table `acore_world`.`forge_talent_tabs` add `role` tinyint(1) default 0 after `description`;
+Alter table `acore_world`.`forge_talent_tabs` add `spellString` varchar(255) after `role`;

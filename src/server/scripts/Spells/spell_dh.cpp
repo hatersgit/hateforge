e5475bd3b9a107029898f16355b5625cbf939e5e
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2023-12-10 18:56:16 +0100

    Add files via upload

--- src/server/scripts/Spells/spell_dh.cpp
+++ src/server/scripts/Spells/spell_dh.cpp
@@ -12,15 +12,11 @@
 
 enum DemonHunterSpells
 {
-    SPELL_DH_DESPAIR_DEBUFF                 = 1410014,
-    SPELL_DH_DESPAIR_TALENT                 = 1410013,
-    SPELL_DH_SIGIL_OF_FLAME_EXPLOSION       = 1410004,
-    SPELL_DH_SIGIL_OF_FLAME_ENERGIZE        = 1410005,
-    SPELL_DH_SIGIL_OF_MISERY_EXPLOSION      = 1410007,
-
+    SPELL_DH_SIGIL_OF_FLAME_EXPLOSION = 1410004,
 };
 
-// 1410003 - Sigil of Flame
+// 204596 - Sigil of Flame
+// MiscId - 6039
 struct at_dh_sigil_of_flame : public AreaTriggerAI
 {
     at_dh_sigil_of_flame(AreaTrigger* areatrigger) : AreaTriggerAI(areatrigger) { }
@@ -28,36 +24,14 @@ struct at_dh_sigil_of_flame : public AreaTriggerAI
     void OnRemove() override
     {
         Unit* caster = at->GetCaster();
-
         if (!caster)
             return;
 
         caster->CastSpell(at->GetPositionX(), at->GetPositionY(), at->GetPositionZ(), SPELL_DH_SIGIL_OF_FLAME_EXPLOSION, true);
-        caster->CastSpell(caster, SPELL_DH_SIGIL_OF_FLAME_ENERGIZE, true);
-    }
-};
-
-// 1410006 - Sigil of Misery
-struct at_dh_sigil_of_misery : AreaTriggerAI
-{
-    at_dh_sigil_of_misery(AreaTrigger* areatrigger) : AreaTriggerAI(areatrigger) { }
-
-    void OnRemove() override
-    {
-        Unit* caster = at->GetCaster();
-
-        if (!caster)
-            return;
-
-        caster->CastSpell(at->GetPositionX(), at->GetPositionY(), at->GetPositionZ(), SPELL_DH_SIGIL_OF_MISERY_EXPLOSION, true);
-
-        if (caster->HasAura(SPELL_DH_DESPAIR_TALENT))
-            caster->CastSpell(at->GetPositionX(), at->GetPositionY(), at->GetPositionZ(), SPELL_DH_DESPAIR_DEBUFF, true);
     }
 };
 
 void AddSC_demonhunter_spell_scripts()
 {
     RegisterAreaTriggerAI(at_dh_sigil_of_flame);
-    RegisterAreaTriggerAI(at_dh_sigil_of_misery);
 }

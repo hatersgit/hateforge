4404a1fd23bdba31ca46d916884966c9f730abfa
hater <hater.rip@proton.me>
2024-01-14 17:39:38 -0500

    add action sql

---
+++ data/sql/custom/db_world/20240114_01.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `forge_character_action` (
+  `guid` int unsigned NOT NULL DEFAULT '0',
+  `spec` tinyint unsigned NOT NULL DEFAULT '0',
+  `loadout` int unsigned NOT NULL DEFAULT '1',
+  `button` tinyint unsigned NOT NULL DEFAULT '0',
+  `action` int unsigned NOT NULL DEFAULT '0',
+  `type` tinyint unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`guid`,`spec`,`loadout`,`button`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;

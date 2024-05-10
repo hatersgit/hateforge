8b0d8a6ee435cd69410ffeec81fe980372dce498
hater <hater.rip@proton.me>
2024-05-10 11:31:58 -0400

    fix point sql

--- data/sql/custom/db_characters/20240316_01.sql
+++ data/sql/custom/db_characters/20240316_01.sql
@@ -7,3 +7,13 @@ CREATE TABLE `encounter_loot_lockout` (
   `resettime` int unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`char`, `map`, `difficulty`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+DELETE FROM acore_characters.forge_character_points where guid = 4294967295;
+INSERT INTO acore_characters.forge_character_points (guid, `type`, spec, sum, max) VALUES(4294967295, 8, 4294967295, 0, 21);
+INSERT INTO acore_characters.forge_character_points (guid, `type`, spec, sum, max) VALUES(4294967295, 7, 4294967295, 0, 25);
+INSERT INTO acore_characters.forge_character_points (guid, `type`, spec, sum, max) VALUES(4294967295, 6, 4294967295, 0, 0);
+INSERT INTO acore_characters.forge_character_points (guid, `type`, spec, sum, max) VALUES(4294967295, 4, 4294967295, 17, 17);
+INSERT INTO acore_characters.forge_character_points (guid, `type`, spec, sum, max) VALUES(4294967295, 3, 4294967295, 0, 20);
+INSERT INTO acore_characters.forge_character_points (guid, `type`, spec, sum, max) VALUES(4294967295, 0, 4294967295, 1, 60);
